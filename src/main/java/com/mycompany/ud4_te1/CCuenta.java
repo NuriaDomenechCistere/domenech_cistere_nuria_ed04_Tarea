49a1665ffe00b773ac31c0c823861985231b5a4b
nuriadomenech <ndomenech@birt.eus>
2023-05-09 19:56:22 +0200

    Versión 3. Tercer commit del proyecto. Se han realizado los siguientes cambios en la clase CCuenta:     - Borrar los métodos que no se usan en el programa siendo comprobados con Safely Delete.     - Renombrar el método estado() por getSaldo() para que se entienda el getter.     - Encapsular los atributos de la clase CCuenta para añadir los setters y getters que faltaban.

--- src/main/java/com/mycompany/ud4_te1/CCuenta.java
+++ src/main/java/com/mycompany/ud4_te1/CCuenta.java
@@ -7,6 +7,55 @@ public class CCuenta {
     private double saldo;
     private double tipoInteres;
 
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the cuenta
+     */
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    /**
+     * @param cuenta the cuenta to set
+     */
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * @return the tipoInteres
+     */
+    public double getTipoInteres() {
+        return tipoInteres;
+    }
+
+    /**
+     * @param tipoInteres the tipoInteres to set
+     */
+    public void setTipoInteres(double tipoInteres) {
+        this.tipoInteres = tipoInteres;
+    }
+
 
     public CCuenta() {
     }
@@ -17,15 +66,9 @@ public class CCuenta {
         saldo = sal;
     }
 
-    public void asignarNombre(String nom) {
-        setNombre(nom);
-    }
 
-    public String obtenerNombre() {
-        return getNombre();
-    }
 
-    public double estado() {
+    public double getSaldo() {
         return saldo;
     }
 
@@ -39,28 +82,13 @@ public class CCuenta {
     public void retirar(double cantidad) throws Exception {
         if (cantidad <= 0)
             throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado() < cantidad + 2)
+        if (getSaldo() < cantidad + 2)
             throw new Exception ("No se hay suficiente saldo");
         saldo = saldo - (cantidad + 2);
     }
 
-    public String obtenerCuenta() {
-        return cuenta;
-    }
 
-    public String getNombre() {
-        return nombre;
-    }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
 
-    public double getTipoInteres() {
-        return tipoInteres;
-    }
 
-    public void setTipoInteres(double tipoInteres) {
-        this.tipoInteres = tipoInteres;
-    }
 }
