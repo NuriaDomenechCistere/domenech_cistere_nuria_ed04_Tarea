ae8939ce469c893273644a68ffed07e3d6bd6999
nuriadomenech <ndomenech@birt.eus>
2023-05-09 20:05:00 +0200

    Versión 5. Quinto commit del proyecto. Se han realizado los siguientes cambios en la clase CCuenta:     - Se ha añadido la constante COMISION en el valor 2 del método retirar.

--- src/main/java/com/mycompany/ud4_te1/CCuenta.java
+++ src/main/java/com/mycompany/ud4_te1/CCuenta.java
@@ -82,10 +82,11 @@ public class CCuenta {
     public void retirar(double cantidad) throws Exception {
         if (cantidad <= 0)
             throw new Exception ("No se puede retirar una cantidad negativa");
-        if (getSaldo() < cantidad + 2)
+        if (getSaldo() < cantidad + COMISION)
             throw new Exception ("No se hay suficiente saldo");
-        saldo = saldo - (cantidad + 2);
+        saldo = saldo - (cantidad + COMISION);
     }
+    private static final int COMISION = 2;
 
 
 
