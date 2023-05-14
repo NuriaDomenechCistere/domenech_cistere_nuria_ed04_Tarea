3e5444fa668d02e9b9ff0b7179c71c44814f2d8a
nuriadomenech <ndomenech@birt.eus>
2023-05-14 20:52:39 +0200

    Versión 6. Sexto commit del proyecto. Se han realizado los siguientes cambios en la clase CCuenta:     - Se han añadido comentarios Javadoc a toda la clase. 	- Se ha descrito la clase en el comentario. 	- Se han documentado los métodos getSaldo e ingresar.

--- src/main/java/com/mycompany/ud4_te1/CCuenta.java
+++ src/main/java/com/mycompany/ud4_te1/CCuenta.java
@@ -1,5 +1,11 @@
 package com.mycompany.ud4_te1;
 
+/**
+  * La clase CCuenta crea una cuenta a través de diferentes atributos: el nombre de la persona dueña de la cuenta, el número de cuenta, el saldo que tiene y el tipo de Interés que se le aplica. <b>Tiene la funcionalidad de ingresar y retirar dinero de la misma.</b>
+ * @author Núria
+ * @version 6.0
+
+ */
 public class CCuenta {
 
     private String nombre;
@@ -56,22 +62,40 @@ public class CCuenta {
         this.tipoInteres = tipoInteres;
     }
 
-
+    /**
+     *
+     */
     public CCuenta() {
     }
 
+    /**
+     *
+     * @param nom
+     * @param cue
+     * @param sal
+     * @param tipo
+     */
     public CCuenta(String nom, String cue, double sal, double tipo) {
         nombre = nom;
         cuenta = cue;
         saldo = sal;
     }
 
+    /**
+      * <u>Retorna el saldo de la cuenta</u> en el momento en que se llama al método.
+     * @return Retorna el saldo en cuenta actual. En la clase Main se utiliza para devolver el valor en cuanto se ha llamado al método ingresar o retirar y se ha cambiado ese valor.
+     * @see com.mycompany.ud4_te1.Main
 
-
+     */
     public double getSaldo() {
         return saldo;
     }
 
+    /**
+      *Calcula la cantidad de dinero en la cuenta que queda después de realizar un movimiento de ingreso. De esta manera, se suma al saldo que había el valor que se introduce como cantidad.
+     * @param cantidad Es un valor númerico que se suma al saldo de la cuenta en el momento de llamar al método.
+     * @throws Exception Lanza una excepción si el valor del parámetro cantidad es menor que 0 ya que no es posible ingresar una cantidad negativa puesto que es <b>imposible</b> ingresar dinero en negativo.
+     */
     public void ingresar(double cantidad) throws Exception {
         if (cantidad < 0) {
             throw new Exception("No se puede ingresar una cantidad negativa");   
@@ -79,6 +103,11 @@ public class CCuenta {
         saldo = saldo + cantidad;
     }
 
+    /**
+     *
+     * @param cantidad
+     * @throws Exception
+     */
     public void retirar(double cantidad) throws Exception {
         if (cantidad <= 0)
             throw new Exception ("No se puede retirar una cantidad negativa");
